030841ed7d722e5159427d29e8d4acb1f97e301d
amparo <114286711+amparoamparo@users.noreply.github.com>
2023-06-17 19:31:35 +0100

    create logo components

---
+++ src/components/logos/AnimatedLogo.jsx
@@ -0,0 +1,5 @@
+const AnimatedLogo = () => {
+  return <h1>amparo.codes</h1>;
+};
+
+export default AnimatedLogo;
---
+++ src/components/logos/StaticLogo.jsx
@@ -0,0 +1,5 @@
+const StaticLogo = () => {
+  return <a href="/">amparo.codes</a>;
+};
+
+export default StaticLogo;
