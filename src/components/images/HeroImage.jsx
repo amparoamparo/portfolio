e7a7059d440f103be69a316175f195a675e77700
amparo <114286711+amparoamparo@users.noreply.github.com>
2023-06-17 20:40:02 +0100

    add hero images and create hero image component

---
+++ src/components/images/HeroImage.jsx
@@ -0,0 +1,21 @@
+const HeroImage = ({ theme }) => {
+  return (
+    <>
+      {theme === "dark" ? (
+        <img
+          src="/assets/images/hero-image-dark-mode.png"
+          alt="A profile silhouette illustration of Amparo on a dark blue background, in an arched shape."
+          width={480}
+        />
+      ) : (
+        <img
+          src="/assets/images/hero-image-light-mode.png"
+          alt="A profile illustration of Amparo in black and white, on a yellow background, in an arched shape."
+          width={480}
+        />
+      )}
+    </>
+  );
+};
+
+export default HeroImage;
