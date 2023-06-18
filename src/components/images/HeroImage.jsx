216f6168033402f259e1236838f55e2ca9e81704
amparo <114286711+amparoamparo@users.noreply.github.com>
2023-06-18 17:32:59 +0100

    add height to hero image in html to prevent CLS

--- src/components/images/HeroImage.jsx
+++ src/components/images/HeroImage.jsx
@@ -6,12 +6,14 @@ const HeroImage = ({ theme }) => {
           src="/assets/images/hero-image-dark-mode.png"
           alt="A profile silhouette illustration of Amparo on a dark blue background, in an arched shape."
           width={480}
+          height={536}
         />
       ) : (
         <img
           src="/assets/images/hero-image-light-mode.png"
           alt="A profile illustration of Amparo in black and white, on a yellow background, in an arched shape."
           width={480}
+          height={536}
         />
       )}
     </>
