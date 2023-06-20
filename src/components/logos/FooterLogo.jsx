6804a04c15fed6db731a7906463c0dfac609b29c
amparo <114286711+amparoamparo@users.noreply.github.com>
2023-06-20 19:31:24 +0100

    style header logo and footer logo

--- src/components/logos/FooterLogo.jsx
+++ src/components/logos/FooterLogo.jsx
@@ -1,9 +1,14 @@
 /**
  * Logo component displayed in the footer. It's static and links to the home page.
  */
+import "../../scss/logo.scss";
 
 const FooterLogo = () => {
-  return <a href="/">amparo.codes</a>;
+  return (
+    <a href="/" className="footer__logo">
+      amparo.codes
+    </a>
+  );
 };
 
 export default FooterLogo;
--- src/components/logos/HeaderLogo.jsx
+++ src/components/logos/HeaderLogo.jsx
@@ -2,8 +2,16 @@
  * Logo component displayed in the header. It's animated and links to the home page.
  */
 
+import "../../scss/logo.scss";
+
 const HeaderLogo = () => {
-  return <h1>amparo.codes</h1>;
+  return (
+    <div className="header__logo">
+      <h1>
+        <a href="/">amparo.codes</a>
+      </h1>
+    </div>
+  );
 };
 
 export default HeaderLogo;
