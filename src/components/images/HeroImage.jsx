5f25204ba99c8ba82f8b6ae20b9ba435fca07bc0
amparo <114286711+amparoamparo@users.noreply.github.com>
2023-06-23 20:54:29 +0100

    replace png image for webp image

--- src/components/images/HeroImage.jsx
+++ src/components/images/HeroImage.jsx
@@ -5,7 +5,7 @@ const HeroImage = ({ theme }) => {
     <>
       {theme === "dark" ? (
         <img
-          src="/assets/images/hero-image-dark-mode.png"
+          src="/assets/images/hero-image-dark-mode.webp"
           alt="A profile silhouette illustration of Amparo on a dark blue background, in an arched shape."
           width={480}
           height="100%"
@@ -14,7 +14,7 @@ const HeroImage = ({ theme }) => {
       ) : (
         <div className="hero-image">
           <img
-            src="/assets/images/hero-image-light-mode.png"
+            src="/assets/images/hero-image-light-mode-mobile.webp"
             alt="A profile illustration of Amparo in black and white, on a yellow background, in an arched shape."
             width={480}
             height="100%"
