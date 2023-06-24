e483f785c5e58422e0531433da113cc97114251a
amparo <114286711+amparoamparo@users.noreply.github.com>
2023-06-24 12:17:39 +0100

    attempt to fix dependencies issues

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,16 +1,8 @@
 import { defineConfig } from "astro/config";
-import react from "@astrojs/react";
 
-import partytown from "@astrojs/partytown";
+import react from "@astrojs/react";
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [
-    react(),
-    partytown({
-      config: {
-        forward: ["dataLayer.push"],
-      },
-    }),
-  ],
+  integrations: [react()],
 });
