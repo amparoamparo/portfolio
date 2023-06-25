2d7ffe32889a94e88e0028cf1464fcbe355871fa
amparo <114286711+amparoamparo@users.noreply.github.com>
2023-06-25 12:51:09 +0100

    update image size in markup

--- src/components/images/HeroImage.jsx
+++ src/components/images/HeroImage.jsx
@@ -6,15 +6,15 @@ const HeroImage = () => {
       <img
         src="/assets/images/hero-image-light-mode-mobile.webp"
         alt="A profile illustration of Amparo in black and white, on a yellow background, in an arched shape."
-        width={480}
-        height="100%"
+        width={200}
+        height={223.333}
         className="hero-image__img-xs"
       />
       <img
         src="/assets/images/hero-image-light-mode.webp"
         alt="A profile illustration of Amparo in black and white, on a yellow background, in an arched shape."
         width={480}
-        height="100%"
+        height={536}
         className="hero-image__img-lg"
       />
     </>
